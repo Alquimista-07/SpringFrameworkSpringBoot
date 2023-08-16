c5ab689074a48959c1cc1316eb8d80fe2c1d4f8c
Alquimista-07 <jdca.07@gmail.com>
2023-08-16 11:22:51 -0500

    23. Obtener varios parámetros de la URL con @RequestParam

--- workspace/Sección 2 - Spring MVC/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ workspace/Sección 2 - Spring MVC/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
@@ -31,6 +33,52 @@ public class EjemploParamsController {
 	@GetMapping("/")
 	public String index() {
 		return "params/index";
+	}
+	
+	@GetMapping("/mix-params")
+	/*
+	 * NOTA: Método para enviar más de un parámetro, con strings y enteros.
+	 *       Adicionalmente como no especificamos el nombre del parámetro
+	 *       a través del name, por lo tanto tenemos que enviarlo tal cual 
+	 *       como llamamos la variable del argumento en el método
+	 */
+	public String param( @RequestParam String saludo, @RequestParam Integer numero,  Model model ) {
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo +"' y el número es '" + numero + "'" );
+		return "params/ver";
+		
+	}
+	
+	/*
+	 * NOTA: El siguiente método es otra forma de recibir parámetros en el controlador
+	 *       usando el HttpServletRequest
+	 */
+	@GetMapping("/mix-params-request")
+	public String param( HttpServletRequest request, Model model ) {
+		/*
+		 *  NOTA: Y a través de request obtenemos los valores enviados en los parámetros
+		 *        usando el métod getInitParameter al cual le indicamos por argumento
+		 *        el nombre del parámetro.
+		 */
+		String saludo = request.getParameter("saludo");
+		
+		/*
+		 * NOTA: Como el getParameter regresa un string tenemos que parsear para convertir a entero
+		 *       el valor, adicionalmente para evitar errores en caso de que se envíe de forma errada
+		 *       y se mande un texto no se de un error, para ello usamos el try catch
+		 */
+		Integer numero = null;
+		try {			
+			numero = Integer.parseInt(request.getParameter("numero"));
+		}
+		// Captura le excepción y le agrega un valor por defecto cuando se da un error
+		catch(NumberFormatException err) {
+			System.out.println("Error: " + err);
+			numero = 0;
+		}
+		
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo +"' y el número es '" + numero + "'" );
+		
+		return "params/ver";
 	}
 
 }
