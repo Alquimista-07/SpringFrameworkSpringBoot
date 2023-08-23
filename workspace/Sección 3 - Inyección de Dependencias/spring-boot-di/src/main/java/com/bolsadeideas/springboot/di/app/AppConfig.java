490eb6a7293943c3fabb752b2ea63eb10b53fdcc
Alquimista-07 <jdca.07@gmail.com>
2023-08-23 13:34:55 -0500

    Eliminación y refactor para separar el ejemplo de factura en un proyecto aparte

--- workspace/Sección 3 - Inyección de Dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,14 +1,9 @@
 package com.bolsadeideas.springboot.di.app;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
-import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio2;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio2;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -57,30 +52,5 @@ public class AppConfig {
 		return new MiServicioComplejo();
 	}
 	
-	/*
-	 * NOTA: Método para que retorne un list de los items 
-	 *       de la factura.
-	 *       Adicionalmente le colocamos un nombre al Bean 
-	 *       en caso de que tengamos más de in Bean del tipo 
-	 *       List<ItemFactura>
-	 */
-	@Bean("itemsFactura")
-	public List<ItemFactura> registrarItemsFactura(){
-		/*
-		 * NOTA: Creamos unos objetos de producto, de itemas factura, asignamos
-		 *       los productos y los guardamos en un arraylist
-		 */
-		Producto prod1 = new Producto("Cámara Sony", 100);
-		Producto prod2 = new Producto("Bicicleta Bianchi aro 26", 200);
-		
-		ItemFactura linea1 = new ItemFactura(prod1, 2);
-		ItemFactura linea2 = new ItemFactura(prod2, 4);
-		
-		/*
-		 * NOTA: Usando Arrays praticamente sería lo mismo que si crearamos la instancia de
-		 * ArrayList = new ArrayList y agregaramos los elementos usando el .add()
-		 */      
-		return Arrays.asList(linea1, linea2);
-	}
 
 }
