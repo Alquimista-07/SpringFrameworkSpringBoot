8abee7137e23544915e2edc03f2504019895704d
Alquimista-07 <jdca.07@gmail.com>
2023-08-23 09:52:54 -0500

    41. Ejemplo Factura y relaciones entre sus componentes

---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+public class ItemFactura {
+	
+	// Atributos
+	private Producto producto;
+	
+	private Integer cantidad;
+	
+	// Getters y Setters
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+
+}
---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+public class Producto {
+	
+	// Atributos
+	private String nombre;
+	
+	private Integer precio;
+	
+	// Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+	
+	
+
+}
