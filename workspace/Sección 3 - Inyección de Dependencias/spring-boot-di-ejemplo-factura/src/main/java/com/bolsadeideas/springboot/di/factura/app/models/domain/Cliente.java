e6968f0a1e5baf693860205aeeaecbeea9e73ee3
Alquimista-07 <jdca.07@gmail.com>
2023-08-23 14:03:34 -0500

    41. Ejemplo Factura y relaciones entre sus componentes

---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di-ejemplo-factura/src/main/java/com/bolsadeideas/springboot/di/factura/app/models/domain/Cliente.java
@@ -0,0 +1,38 @@
+package com.bolsadeideas.springboot.di.factura.app.models.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cliente {
+
+	// Atributos
+	/*
+	 * NOTA: El nombre y el apellido lo podemos guardar en un archivo properties,
+	 * por lo tanto los inyectamos usando el decorador @Value para inyectar sus
+	 * correspondientes valores.
+	 */
+	@Value("${cliente.nombre}")
+	private String nombre;
+
+	@Value("${cliente.apellido}")
+	private String apellido;
+
+	// Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+}
---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di-ejemplo-factura/src/main/java/com/bolsadeideas/springboot/di/factura/app/models/domain/Factura.java
@@ -0,0 +1,54 @@
+package com.bolsadeideas.springboot.di.factura.app.models.domain;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Factura {
+
+	// Atributos
+	/*
+	 * NOTA: El titulo de la factura lo podemos guardar en un archivo properties,
+	 * por lo tanto los inyectamos usando el decorador @Value para inyectar sus
+	 * correspondientes valores.
+	 */
+	@Value("${descripcion.factura}")
+	private String descipción;
+
+	/*
+	 * NOTA: Inyectamos el cliente usando el @Autowired
+	 */
+	@Autowired
+	private Cliente cliente;
+
+	private List<ItemFactura> items;
+
+	// Getters y Setters
+	public String getDescipción() {
+		return descipción;
+	}
+
+	public void setDescipción(String descipción) {
+		this.descipción = descipción;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+}
