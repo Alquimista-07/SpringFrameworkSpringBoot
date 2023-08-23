944ca0cd28d1479c0d60f3a9b46253aef3fe2379
Alquimista-07 <jdca.07@gmail.com>
2023-08-23 13:55:11 -0500

    Creación proyecto ejemplo factura

---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di-ejemplo-factura/src/main/java/com/bolsadeideas/springboot/di/factura/app/SpringBootDiEjemploFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.factura.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiEjemploFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiEjemploFacturaApplication.class, args);
+	}
+
+}
---
+++ workspace/Sección 3 - Inyección de Dependencias/spring-boot-di-ejemplo-factura/src/test/java/com/bolsadeideas/springboot/di/factura/app/SpringBootDiEjemploFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.factura.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiEjemploFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
