4718b02607842329ec1322c2ea6dd2089b6c3f4c
Alquimista-07 <jdca.07@gmail.com>
2023-08-24 11:33:50 -0500

    51. Método handler para recibir y procesar los datos enviados

--- workspace/Sección 4 - Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ workspace/Sección 4 - Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -13,6 +14,7 @@ public class FormController {
 	 */
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario Usuario");
 		return "form";
 	}
 	
@@ -20,7 +22,26 @@ public class FormController {
 	 * NOTA: Método para enviar y procesar los datos
 	 */
 	@PostMapping("/form")
-	public String procesarFormulario(Model model) {
+	/*
+	 * NOTA: Para capturar los valores del formulario usamos la anotación
+	 *       @RequestParam y se tiene que indicar con el mismo nombre como
+	 *       se indico en el atributo name en el formulario html.
+	 *       
+	 *       Recordemos que también podemos usar el name o el value del 
+	 *       @RequestParam para indicar el nombre del input, por ejemplo
+	 *       
+	 *       @RequestParam(name="email") String correo
+	 */
+	public String procesarFormulario(Model model, @RequestParam(name = "username") String username,
+								                  @RequestParam String pwd,
+								                  @RequestParam String email) {
+		// Pasamos los datos a la vista con model
+		model.addAttribute("titulo", "Resultador Form");
+		
+		model.addAttribute("username", username);
+		model.addAttribute("pwd", pwd);
+		model.addAttribute("email", email);
+		
 		return "resultado";
 	}
 	
