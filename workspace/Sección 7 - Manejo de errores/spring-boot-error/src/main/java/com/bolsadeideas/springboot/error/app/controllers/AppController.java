2f6c3732f2fc7bb256d755be5d5813d99b18ee98
Alquimista-07 <jdca.07@gmail.com>
2023-09-09 11:12:56 -0500

    108. Personalizando error 404 y 500

--- workspace/Sección 7 - Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ workspace/Sección 7 - Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
 	
 	@GetMapping("/index")
+	// Colocamos una variable llamada valor que no se usa pero que nos va a servir para 
+	// forzar un error, y anotamos con @SuppressWarnings() para omitir el warning que nos 
+	// indica que la variable no se esta usando.
+	@SuppressWarnings("unused")
 	public String index () {
+		Integer valor = 100 / 0;
 		return "index";
 	}
 
