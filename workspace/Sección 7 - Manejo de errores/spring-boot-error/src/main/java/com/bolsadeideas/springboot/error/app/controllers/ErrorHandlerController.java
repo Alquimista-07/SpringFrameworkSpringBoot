9635ab593e94e62b2f785100c14cb34f473d9192
Alquimista-07 <jdca.07@gmail.com>
2023-09-09 12:14:45 -0500

    109. Anotación @ControllerAdvice para manejo de excepciones

---
+++ workspace/Sección 7 - Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,34 @@
+package com.bolsadeideas.springboot.error.app.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// NOTA: Esta clase va a ser un controlador personalizado que nos va a permitir manejar errores como por
+//       ejemplo el ArithmeticException que se genera al dividir por cero. Con la diferencia es que este
+//       controlador no se anota como lo haríamos normalmente con el @Controller sino que se anota con el 
+//       @ControllerAdvice y es bastante importante anotar de esta forma ya que esta anotación indica que es 
+//       para manejar errores y excepciones capturandolas para manejarlas. Otra diferencia es que acá no se
+//       mapea a una ruta como se hace con un controlador normal sino que se mapea a una excepción y se retorna
+//       a una vista.
+@ControllerAdvice
+public class ErrorHandlerController {
+
+	// NOTA: Como se mencionó anteriormente acá no se mapea a una ruta sino a una excepción
+	//       por lo tanto anotamos con @ExceptionHandler() y le pasamos como argumento la
+	//       excepción, que en este caso la división por cero si nos fijamos en el trace 
+	//       que tenemos muestra que es un ArithmeticException. Adicionalmente podemos pasar
+	//       más de una excepción indicandola como un arreglo
+	@ExceptionHandler(ArithmeticException.class)
+	public String aritmeticaError (ArithmeticException ex, Model model) {
+		model.addAttribute("error", "Error de aritmética");
+		model.addAttribute("message", ex.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "error/aritmetica";
+	}
+	
+}
