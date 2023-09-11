28e7249becfd80c4c4822c98d62cda4098f481c6
Alquimista-07 <jdca.07@gmail.com>
2023-09-11 10:05:14 -0500

    124. Agregando crear en Repositorio Dao y Controlador

--- workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -25,6 +28,26 @@ public class ClienteController {
 		// y se lo pasamos a la vista para mostrarlo
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
+	}
+	
+	// Metodo para mostrar el formulario
+	@RequestMapping(value = "/form", method = RequestMethod.GET)
+	// NOTA: Acá no usamos el Model convencional para pasar datos a la vista sino que usamos un map
+	//       pero funcionan igual, es decir, son para pasar datos a la vista.
+	public String crear(Map<String, Object> model) {
+		
+		Cliente cliente = new Cliente();
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
+		return "form";
+	}
+	
+	// Método para procesar o enviar los datos del formulario
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String guardar( Cliente cliente ) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
 	}
 
 }
--- workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -45,4 +45,10 @@ public class ClienteDaoImpl implements IClienteDao {
 		return em.createQuery("from Cliente").getResultList();
 	}
 
+	@Override
+	@Transactional
+	public void save(Cliente cliente) {
+		em.persist(cliente);		
+	}
+
 }
