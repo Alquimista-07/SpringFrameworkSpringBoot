288337bcdb250c37b8d90fe48131dd962ee9834a
Alquimista-07 <jdca.07@gmail.com>
2023-09-12 09:08:51 -0500

    Preparación para implementar la interface CrudRepository propia de springboot

--- workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
+++ workspace/Sección 8 - Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -2,16 +2,16 @@ package com.bolsadeideas.springboot.app.models.dao;
 
 import java.util.List;
 
+// NOTA: La documentación oficial la encontramos referente a la implementación, métodos y demás bondades que nos
+//       brinda la interface CrudRepository propia de Springboot y que nos permite implementar el CRUD de una manera
+//       más sencilla la encontramos en el siguiente enlace: 
+//
+//       https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories
+//
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 public interface IClienteDao {
 	
-	public List<Cliente> findAll();
 	
-	public void save(Cliente cliente);
-	
-	public Cliente findOne(Long id);
-	
-	public void delete(Long id);
-
 }
