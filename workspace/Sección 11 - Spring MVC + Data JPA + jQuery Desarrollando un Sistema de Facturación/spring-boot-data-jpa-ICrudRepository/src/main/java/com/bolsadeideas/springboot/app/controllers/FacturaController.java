e5a478e6c41c6d179f2e0eff0ed865b9ca902a7c
Alquimista-07 <jdca.07@gmail.com>
2023-09-17 15:55:12 -0500

    187. Eliminando la factura y sus líneas

--- workspace/Sección 11 - Spring MVC + Data JPA + jQuery Desarrollando un Sistema de Facturación/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
+++ workspace/Sección 11 - Spring MVC + Data JPA + jQuery Desarrollando un Sistema de Facturación/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
@@ -112,5 +112,21 @@ public class FacturaController {
 		
 		return "factura/ver";
 	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eleiminar( @PathVariable(value = "id") Long id, RedirectAttributes flash ) {
+		
+		Factura factura = clienteService.findFacturaById(id);
+		
+		if ( factura != null ) {
+			clienteService.deleteFactura(id);
+			flash.addFlashAttribute("success", "Factura eliminada con éxito!");
+			return "redirect:/ver/" + factura.getCliente().getId();
+		}
+
+		flash.addFlashAttribute("error", "La factura no existe en la base de datos, no se pudo eliminar");
+		return "redirect:/listar";
+		
+	}
 
 }
