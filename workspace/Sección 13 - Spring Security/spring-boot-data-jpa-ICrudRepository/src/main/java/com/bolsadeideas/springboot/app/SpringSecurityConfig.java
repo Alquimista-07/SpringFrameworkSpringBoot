c4377b2d15401f84bda06557027ec908d04f1629
Alquimista-07 <jdca.07@gmail.com>
2023-09-18 11:16:32 -0500

    202. Añadiendo configuración para el formLogin y logout

--- workspace/Sección 13 - Spring Security/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ workspace/Sección 13 - Spring Security/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.bolsadeideas.springboot.app;
 
+import javax.lang.model.element.ModuleElement.Directive;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +13,8 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 
 @Configuration
 public class SpringSecurityConfig  {
@@ -73,7 +78,10 @@ public class SpringSecurityConfig  {
 				
 				.anyRequest().authenticated();
 				
-		});
+				// Añadimos el formulario de login;
+				
+		}).formLogin(Customizer.withDefaults())
+		  .logout(logout -> logout.addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES))));
 		
 		return http.build();
 		
