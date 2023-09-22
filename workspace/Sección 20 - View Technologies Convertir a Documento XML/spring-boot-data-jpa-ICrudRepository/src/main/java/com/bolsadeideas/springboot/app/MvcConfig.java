6ac9f31a274005d951d243ec6936db99c863b436
Alquimista-07 <jdca.07@gmail.com>
2023-09-22 10:10:57 -0500

    258. Configuración y creando la clase XML Wrapper

--- workspace/Sección 20 - View Technologies Convertir a Documento XML/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ workspace/Sección 20 - View Technologies Convertir a Documento XML/spring-boot-data-jpa-ICrudRepository/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -96,6 +97,20 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	// Método que se encarga del serializar un objeto en un XML
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		
+		marshaller.setClassesToBeBound(new Class[] {
+			// Y dentro de este arreglo indicamos la clase wrapper pasandola con su package
+			com.bolsadeideas.springboot.app.view.xml.ClienteList.class
+		});
+		
+		return marshaller;
 	}
 	
 	
