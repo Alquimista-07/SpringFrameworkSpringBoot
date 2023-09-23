8b2b9d3d1b4032cdc4830a9871d34a3aaf39b474
Alquimista-07 <jdca.07@gmail.com>
2023-09-23 12:20:11 -0500

    276. Generando el JWT

--- workspace/Sección 23 - Spring Security Autenticación basada en JWT/spring-boot-jwt/src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ workspace/Sección 23 - Spring Security Autenticación basada en JWT/spring-boot-jwt/src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,12 +1,23 @@
 package com.bolsadeideas.springboot.app.auth.filter;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -54,6 +65,39 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 		
 		return authenticationManager.authenticate(authToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		
+		String token = Jwts.builder()
+				.setSubject( username )
+				.signWith(SignatureAlgorithm.HS512, "Alguna clave Secreta 1234567890".getBytes())
+				.compact();
+		
+		// Pasamos el token en la cabecera de la respuesta.
+		// OJO es importante que lo pasemos como Authorization
+		// y Bearer, es decir tal cual esta escrito el código.
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		// Pasamos en forma JSON el principal, el token y un mensaje
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("token", token);
+		// NOTA: El patrón %s permite reemplazar por la data que tiene el segundo parántro del format, en este caso el nombre del usuario
+		body.put( "mensaje", String.format("Hola %s, has iniciado sesión con éxito", username) );
+		
+		// Para pasar los datos a la respuesta tenemos que convertir a JSON unsando el método ObjectMapper
+		// como se muestra a continuación
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		
+		// Indicamos el estado de la petición y el content type
+		response.setStatus(200);
+		response.setContentType("application/json");
+		
 	}
 
 	
