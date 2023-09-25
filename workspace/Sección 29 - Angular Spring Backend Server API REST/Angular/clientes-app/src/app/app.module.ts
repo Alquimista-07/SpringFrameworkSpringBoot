155c4ed9a62364eca5caa105a95328a63eac5a1a
Alquimista-07 <jdca.07@gmail.com>
2023-09-25 12:06:58 -0500

    Agregado código fuente base del frontend para dar inicio a la sección 29

---
+++ workspace/Sección 29 - Angular Spring Backend Server API REST/Angular/clientes-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ClienteService } from './services/cliente.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    ClienteService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
