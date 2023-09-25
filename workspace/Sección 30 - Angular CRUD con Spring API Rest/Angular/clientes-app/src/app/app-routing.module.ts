e58187986406f3f3229b12c6bf6691de38ec9d86
Alquimista-07 <jdca.07@gmail.com>
2023-09-25 17:44:05 -0500

    342. Configurando la ruta y navegación del formulario

--- workspace/Sección 30 - Angular CRUD con Spring API Rest/Angular/clientes-app/src/app/app-routing.module.ts
+++ workspace/Sección 30 - Angular CRUD con Spring API Rest/Angular/clientes-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DirectivaComponent } from './components/directiva/directiva.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { FormComponent } from './components/clientes/form.component';
 
 const routes: Routes = [
   {path: 'directivas', component: DirectivaComponent},
   {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent},
   {path: '', redirectTo: '/clientes', pathMatch: 'full'}
 ];
 
