558c30eb13865c240ffab43c15c6eaa7dcb2a898
Alquimista-07 <jdca.07@gmail.com>
2023-09-26 16:41:51 -0500

    Agregado código fuente base para dar inicio a la sección 34

---
+++ workspace/Sección 34 - Angular Validando form en Spring Backend/Angular/clientes-app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ClienteService } from './services/cliente.service';
+import { FormComponent } from './components/clientes/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    ClienteService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
