fc87b95bd388f1d3062e74171b574809385a988a
Alquimista-07 <jdca.07@gmail.com>
2023-09-26 10:32:50 -0500

    Agregado código fuente base para dar inicio a la sección 31

---
+++ workspace/Sección 31 - Angular Manejo de errores en Backend Spring/Angular/clientes-app/src/app/services/cliente.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+//import { of } from 'rxjs';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//import { CLIENTES } from '../components/clientes/clientes.json';
+import { Cliente } from '../interfaces/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private urlEndpoint: string = 'http://localhost:8080/api/clientes';
+  
+  private httpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  constructor( private http: HttpClient ) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders});
+  }
+
+  getCliente(id: string): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`);
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`, cliente, {headers: this.httpHeaders});
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndpoint}/${id}`, {headers: this.httpHeaders});
+  }
+
+}
---
+++ workspace/Sección 31 - Angular Manejo de errores en Backend Spring/spring-boot-backend-api-rest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,86 @@
+package com.bolsadeideas.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
+
+// Como se había mencionado en secciones anteriores como esta es una aplicación solo backend con API REST 
+// los controladores los anotamos con @RestController y no con @Controller.
+@RestController
+@RequestMapping("/api") //Ruta principal
+// Damos acceso al dominio de la aplicación en Angular para que tenga acceso. Adicionalmente como segundo parámetro 
+// indicando el methods podemos decirle que tipo de peticiones se pueden hacer, pero en este caso no los indicamos 
+// para que por defecto se pueda hacer todo tipo de peticiones. También se pueden colocar restricciones sobre los
+// header, y otras cosas que podemos buscar en internet y ver su documentación.
+@CrossOrigin(origins = {"http://localhost:4200"}) 
+public class ClienteRestController {
+	
+	// Inyectamos el servicio
+	@Autowired
+	private IClienteService clienteService;
+	
+	// Ruta para listar los clientes
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return clienteService.findAll();
+	}
+	
+	// Ruta para obtener un cliente por id
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	// Ruta para crear un cliente.
+	// Hay que tener en cuenta que como la información viene en formato JSON dentro del cuerpo de la petición, tenemos
+	// que indicar que es @RequestBody, adicionalmente anotamos con @ResponseStatus para dar el status 201 de respuesta 
+	// correspondiente a creado a la aplicación que realiza la petición, en este caso el frontend de Angular. También si 
+	// no indicamos el @ResponseStatus por defecto va a ser ok status 200.
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente crear(@RequestBody Cliente cliente) { 
+		return clienteService.save(cliente);
+	}
+	
+	// Ruta para actualizar un cliente
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente actualizar(@RequestBody Cliente cliente, @PathVariable Long id) {
+		
+		// Obtenemos el cliente de la base de datos por su id
+		Cliente clienteActual = clienteService.findById(id);
+		
+		// Pasamos los valores modificados
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		// Persistimos el cliente con los cambios realizados. Esto internamente va a hacer un merge de los
+		// datos para actualizarlos y por lo tanto esto detás de escena se traduce en un update en la base
+		// de datos
+		return clienteService.save(clienteActual);
+		
+	}
+	
+	// Ruta para eliminar un cliente por id
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
+
+}
