c387a6958051436cb89552e539600b75c0401d34
Alquimista-07 <jdca.07@gmail.com>
2023-09-26 10:13:40 -0500

    348. Escribiendo el delete en la clase service y en el componente clientes

--- workspace/Sección 30 - Angular CRUD con Spring API Rest/Angular/clientes-app/src/app/components/clientes/clientes.component.ts
+++ workspace/Sección 30 - Angular CRUD con Spring API Rest/Angular/clientes-app/src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+// Sweet Alert
+import Swal from 'sweetalert2';
+
 import { Cliente } from 'src/app/interfaces/cliente';
 import { ClienteService } from 'src/app/services/cliente.service';
 
@@ -15,6 +19,31 @@ export class ClientesComponent implements OnInit {
   ngOnInit(): void {
     this.clienteService.getClientes()
         .subscribe( clientes => this.clientes = clientes );
+  }
+
+  public delete(cliente: Cliente): void {
+        Swal.fire({
+          title: '¿Está seguro que desea eliminar?',
+          text: `¿Está seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si, eliminarlo'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.clienteService.delete(cliente.id!).subscribe(
+              response => {
+                this.clientes = this.clientes.filter(cli => cli != cliente)
+                Swal.fire(
+                  'Eliminado!',
+                  `El cliente ${cliente.nombre} ha sido eliminado!`,
+                  'success'
+                )
+              }
+            )
+          }
+        });
   }
 
 }
