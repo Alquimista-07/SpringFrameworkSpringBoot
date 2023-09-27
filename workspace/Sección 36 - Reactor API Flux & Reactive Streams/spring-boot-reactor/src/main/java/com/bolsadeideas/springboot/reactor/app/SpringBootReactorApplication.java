1c7e81b13792b10b97903d43cd6cfa4e5eae7ebd
Alquimista-07 <jdca.07@gmail.com>
2023-09-27 10:51:04 -0500

    377. El método subscribe()

--- workspace/Sección 36 - Reactor API Flux & Reactive Streams/spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ workspace/Sección 36 - Reactor API Flux & Reactive Streams/spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,15 +28,34 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+		
 		// Creamos el observable
 		Flux<String> nombres = Flux.just("Andres", "Pedro", "Julián")
 				// El evento doOnNext se ejecuta cada vez que el observador (nombres en este caso) notifica que ha llegado un elemento.
 				// Adicionalmente el :: es una característica de Java 8 que permite abreviar más el código ya que en si eso sería lo mismo
 				// que hacer .doOnNext(elemento -> System.out.println(elemento));
-				.doOnNext(System.out::println);
+				//.doOnNext(System.out::println);
+				
+				// Emulamos un error
+				.doOnNext(e -> {
+					if(e.isEmpty()) {
+						
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+						
+					}					
+					
+					// Si no hubo error continúa.
+					System.out.println(e);
+					
+				});
 		
-		// Como ya sabemos para invocar un observable es necesario subscribirnos
-		nombres.subscribe();
+		// Como ya sabemos para invocar un observable es necesario subscribirnos.
+		// Adicionalmente cuando nos suscribimos podemos manejar otras cosas.
+		//nombres.subscribe(log::info);
+		nombres.subscribe(e -> log.info(e),
+				// Como segundo elemento podemos manejar el error.
+				error -> log.error(error.getMessage()));
 	}
 
 }
