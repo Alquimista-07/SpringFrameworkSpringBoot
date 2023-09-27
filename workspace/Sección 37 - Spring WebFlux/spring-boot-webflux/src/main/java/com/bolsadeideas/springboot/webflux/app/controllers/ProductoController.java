7b1752107b2c682ec241ae76d0bcea9beaf69f7c
Alquimista-07 <jdca.07@gmail.com>
2023-09-27 15:54:16 -0500

    393. Creando controlador y vista reactivos

---
+++ workspace/Sección 37 - Spring WebFlux/spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,30 @@
+package com.bolsadeideas.springboot.webflux.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	private ProductoDao dao;
+	
+	@GetMapping({"/listar", "/"})
+	public String listar(Model model) {
+		
+		Flux<Producto> productos = dao.findAll();
+		
+		model.addAttribute("titulo","Listado de Productos");
+		model.addAttribute("productos", productos);
+		
+		return "listar";
+	}
+
+}
