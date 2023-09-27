9d2b6c31c5c3460e9525a0a6cf8ab6de76c4a299
Alquimista-07 <jdca.07@gmail.com>
2023-09-27 10:39:52 -0500

    376. Creando un Stream Observable

--- workspace/Sección 36 - Reactor API Flux & Reactive Streams/spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ workspace/Sección 36 - Reactor API Flux & Reactive Streams/spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,40 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
+/*
+ * Enlaces de documentaicón:
+ *  
+ *  https://projectreactor.io/docs/core/release/reference/#getting
+ *  https://www.reactive-streams.org/
+ *  https://github.com/reactive-streams/reactive-streams-jvm/tree/v1.0.4
+ *  https://reactivex.io/
+ */
+
+// NOTA: Como la aplicación va a ser solo de consola implementamos al interface CommandLineRunner 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// Creamos el observable
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "Julián")
+				// El evento doOnNext se ejecuta cada vez que el observador (nombres en este caso) notifica que ha llegado un elemento.
+				// Adicionalmente el :: es una característica de Java 8 que permite abreviar más el código ya que en si eso sería lo mismo
+				// que hacer .doOnNext(elemento -> System.out.println(elemento));
+				.doOnNext(System.out::println);
+		
+		// Como ya sabemos para invocar un observable es necesario subscribirnos
+		nombres.subscribe();
 	}
 
 }
