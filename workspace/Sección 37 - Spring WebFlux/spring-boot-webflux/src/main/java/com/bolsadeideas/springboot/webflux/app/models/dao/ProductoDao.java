dffb49d401d926f87809d8a3684626e36f9ed7c1
Alquimista-07 <jdca.07@gmail.com>
2023-09-27 15:30:04 -0500

    390. Importando datos de ejemplo y el operador flatMap

--- workspace/Sección 37 - Spring WebFlux/spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
+++ workspace/Sección 37 - Spring WebFlux/spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 
-public interface IProductoDao extends ReactiveMongoRepository<Producto, String> {
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
 
 	// De la misma manera que con JPA cuando implementamos la interface CrudRepository
 	// acá también podemos tener métodos propios personalizados que interactuen con los 
